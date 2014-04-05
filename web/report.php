2fdaaedd8717d00652561334098f382295155132
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-05 15:05:13 -0400

    Using object variable reference to parsed JSON data.

--- web/report.php
+++ web/report.php
@@ -6,12 +6,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $report_data = json_decode(file_get_contents("php://input"));
     $stmt = $db_conn->prepare("INSERT INTO REPORT (report_type_id, content, time, latitude, longitude, user_id) VALUES(?, ?, ?, ?, ?, ?)");
     $stmt->bind_param('issddi', 
-        $report_data["report_type_id"], 
-        $report_data["content"],
-        $report_data["time"],
-        $report_data["latitude"],
-        $report_data["longitude"],
-        $report_data["user_id"]);
+        $report_data->report_type_id, 
+        $report_data->content,
+        $report_data->time,
+        $report_data->latitude,
+        $report_data->longitude,
+        $report_data->user_id);
     $stmt->execute();
     $report_id = $db_conn->insert_id;
     $stmt->close();
