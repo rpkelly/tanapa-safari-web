8921989cf94654464d985763d9479c98e8a8d921
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-12 14:33:20 -0400

    Modifiied safaridetails.php to return points of interest for a safari.

--- web/safaridetails.php
+++ web/safaridetails.php
@@ -7,13 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
     if (isset($_GET['id'])) {
 
+        $results = array();
+
         // If an ID is sent, retreive that safari's details and return them.
         $stmt = $db_conn->prepare("SELECT * FROM SAFARI_WAYPOINTS WHERE SAFARI_ID = ?");
-        $stmt->bind_param('s', $_GET['id']);
+        $stmt->bind_param('i', $_GET['id']);
         $stmt->execute();
         $stmt->bind_result($waypoint_id, $sequence, $latitude, $longitude, $safari_id);
         while ($stmt->fetch()) {
-            $results["results"][] = array(
+            $results["results"]["waypoints"][] = array(
                 "id" => $waypoint_id,
                 "sequence" => $sequence,
                 "latitude" => $latitude,
@@ -22,6 +24,22 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             );
         }
         $stmt->close();
+
+        $stmt = $db_conn->prepare("SELECT * FROM SAFARI_POINTS_OF_INTEREST WHERE SAFARI_ID = ?");
+        $stmt->bind_param('i', $_GET['id']);
+        $stmt->execute();
+        $stmt->bind_result($poi_id, $name, $latitude, $longitude, $radius);
+        while ($stmt->fetch()) {
+            $results["results"]["points_of_interest"][] = array(
+                "id" => $poi_id,
+                "name" => $name,
+                "latitude" => $latitude,
+                "longitude" => $longitude,
+                "radius" => $radius,
+            );
+        }
+        $stmt->close();
+
         echo json_encode($results);
     }
 
