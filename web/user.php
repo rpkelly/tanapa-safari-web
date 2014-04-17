b437100ddf8befca189462c2dbde93310e1371d8
rpkelly <rpkelly@g.clemson.edu>
2014-04-17 03:43:37 -0400

    user.php now creates a user whenever a get request is sent to it and returns JSON of the user's id

--- web/user.php
+++ web/user.php
@@ -7,17 +7,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     // If no parameters were given, return list of safaris.
     if (empty($_GET)) {
         $results = array("results" => array());
-        $stmt = $db_conn->prepare("INSERT INTO user VALUES ()");
-        $stmt->execute();
-		$stmt = $db_conn->prepare("SELECT LAST_INSERT_ID()");
-        $stmt->bind_result($user_id);
-        while ($stmt->fetch()) {
+        $db_conn->query("INSERT INTO USER VALUES (NULL)");
+        $user_id = $db_conn->insert_id;
             $results["results"][] = array(
                 "id" => $user_id
             );
-        }
-        $stmt->close();
-        echo json_encode($results);
+        $db_conn->close();
+		echo json_encode($results);
 
     }
 }
