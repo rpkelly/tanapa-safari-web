6e03891b01b215c6cf4d45dd8aabdf616dc2d6ab
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-21 11:45:39 -0400

    Fixed bug where report media id was not being set.

--- web/report.php
+++ web/report.php
@@ -4,6 +4,7 @@ require('config/config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $report_data = json_decode(file_get_contents("php://input"));
+    $report_media_id = isset($report_data->media) ? $report_data->media->id : null;
     $stmt = $db_conn->prepare("INSERT INTO REPORT (report_type_id, content, time, latitude, longitude, user_id, report_media_id) VALUES(?, ?, ?, ?, ?, ?, ?)");
     $stmt->bind_param('issddi', 
         $report_data->report_type_id, 
@@ -12,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $report_data->latitude,
         $report_data->longitude,
         $report_data->user_id,
-        isset($report_data->media) ? $report_data->media->id : null);
+        $report_media_id);
     $stmt->execute();
     $report_id = $db_conn->insert_id;
     $stmt->close();
