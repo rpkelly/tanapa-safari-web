50a76a5db9db837615767158c511cfbebb2c02b3
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-21 11:15:00 -0400

    Fixed bug where report media id was not being set.

--- web/report.php
+++ web/report.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $report_data->latitude,
         $report_data->longitude,
         $report_data->user_id,
-        $report_data->report_media_id);
+        $report_data->media->id);
     $stmt->execute();
     $report_id = $db_conn->insert_id;
     $stmt->close();
