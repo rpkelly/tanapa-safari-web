8a569109e3e191ba8fda719c6fadae12406aad4a
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-30 08:02:42 -0400

    Adding media management.

--- web/media_manager.php
+++ web/media_manager.php
@@ -7,10 +7,37 @@
 <html>
     <head>
         <title>TANAPA Safari Admin Panel</title>
-        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        
+        <script>
+            $("#mediaFileInput").on("change", function(e){
+
+                if (e.source.val()) {
+                    //var formData = new FormData($("#poiMediaFileForm")[0]);
+                    $.ajax({
+                        url: 'media.php',  //Server script to process data
+                        type: 'POST',
+                        success: function(data){
+                            var response = JSON.parse(data);
+                            console.log(response);
+                            //rowBeingEdited.find("td:nth-child(5)").html("<a href='" + response.url.substring(1) + "'>" + response.url.substring(1) + "</a>");
+                        },
+                        // Form data
+                        data: e.source.form,
+                        //Options to tell jQuery not to process data or worry about content-type.
+                        cache: false,
+                        contentType: false,
+                        processData: false
+                    });   
+                }
+
+
+            });
+        </script>
+
     </head>
     <body>
         <?php
@@ -63,7 +90,7 @@
                                         <tbody>
                                     </table>
                                     <form id="mediaFileInputForm">
-                                        <input type="file" id="mediaFileInput">
+                                        <input type="file" id="mediaFileInput" name="file">
                                     </form>
                                 </div>
                             </div>
